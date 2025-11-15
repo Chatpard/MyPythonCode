acc19bf8d94beb97186c52ccf689088051fce715
Christian <moenaert@hotmail.com>
2025-11-15 15:28:24 +0100

    file

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,5 +3,16 @@ print("Hello the others")
 print("new branch")
 print("newer branch")
 print("again")
-user_name = input("Give your name ? ")
-print("Your name is", user_name)
+# open file for writing
+with open(file="text.txt", mode="a") as f:
+    while(True):
+        user_name = input("Give your name ? ")
+        if(not user_name):
+            break
+        print("Your name is", user_name)
+        # write to file
+        f.write(user_name)
+        f.write("\n")
+print("This is the result.")
+with open(file="text.txt", mode="r") as f:
+    print(f.read())
